856e9e47b6827f26ac84f3d2df585d08f0839de1
Michal Puchy <puchy.michal@gmail.com>
2024-05-28 13:03:55 +0200

    5c - Rendering the component for tests

--- notesFE/src/components/Note.jsx
+++ notesFE/src/components/Note.jsx
@@ -2,7 +2,7 @@ const Note = ({ note, toggleImportance, onDelete }) => {
   const label = note.important ? 'not important' : 'important'
 
   return (
-    <li className="note">
+    <li className='note'>
       {note.content}
       <button onClick={toggleImportance}>{label}</button>
       <button onClick={() => onDelete(note.id, note.content)}>Delete</button>
