7d34c99721395ed9a87bc75caa978d61deee3eb0
Michal Puchy <puchy.michal@gmail.com>
2024-06-19 17:49:43 +0200

    5c - testing React apps; rendering component for test, test coverage

--- notesFE/src/components/Note.jsx
+++ notesFE/src/components/Note.jsx
@@ -2,8 +2,8 @@ const Note = ({ note, toggleImportance, onDelete }) => {
   const label = note.important ? 'not important' : 'important'
 
   return (
-    <li className='note'>
-      {note.content}
+    <li className='note' data-testid="custom-element">
+      Your awesome note: {note.content}
       <button onClick={toggleImportance}>{label}</button>
       <button onClick={() => onDelete(note.id, note.content)}>Delete</button>
     </li>
--- notesFE/src/components/Note.test.jsx
+++ notesFE/src/components/Note.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
 test('renders content', () => {
@@ -7,8 +8,42 @@ test('renders content', () => {
     important: true
   }
 
+  const { container } = render(<Note note={note} />)
+  const li = container.querySelector('[data-testid="custom-element"')
+
+  //* screen.debug(li) -> Object screen has method debug that can be used to print the HTML of a component to the terminal.
+  expect(li).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  )
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  const mockHandler = vi.fn()
+
+  render(
+    <Note note={note} toggleImportance={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('not important', { exact: false }) //Or const element = await screen.findByText('Does not work anymore :('); careful it returns promise!
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
+})
+
+test('does not render this', () => {
+  const note = {
+    content: 'This is a reminder',
+    important: true
+  }
+
   render(<Note note={note} />)
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
-  expect(element).toBeDefined()
+  const element = screen.queryByText('do not want this thing to be rendered')
+  expect(element).toBeNull()
 })
